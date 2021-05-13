b07f9391e4588f62da194c67514cc19894fd2dfc
Eva <yi.zheng1709@gmail.com>
2021-05-12 23:55:32 -0400

    commit tree b0bd3251110c36e90dcc6a4df56f9460f9f2c8a9

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react'
+
+class EyesOnMe extends Component {
+    render() {
+        return(
+            <div>
+                <button onBlur={ () => {console.log("Hey! Eyes on me!")} } onFocus={ () => {console.log("Good!")} } ></button>
+            </div>
+        )
+    }
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+import React, { Component } from 'react'
+
+class Keypad extends Component {
+    render() {
+        return(
+            <div>
+                <input onKeyUp={() =>console.log("Entering password...")} type="password"></input>
+            </div>
+        )
+    }
+}
+
+export default Keypad
